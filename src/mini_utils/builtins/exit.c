6b0ff585c51bd59d5122bdfe4973946d7d253084
sebferna <116887867+sebasfdezb@users.noreply.github.com>
2025-01-07 18:19:06 +0100

    Add files via upload

---
+++ src/mini_utils/builtins/exit.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sebferna <sebferna@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 17:58:27 by sebferna          #+#    #+#             */
+/*   Updated: 2025/01/07 17:20:22 by sebferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+int	ex_exit(t_data *data, t_parser *node, int i, int fd)
+{
+	ft_printf(fd, "exit\n");
+	if (node->all_cmd[1] != NULL && node->all_cmd[2] != NULL)
+		return (ft_printf(fd, "error exit"));
+	else if (node->all_cmd[1] != NULL)
+	{
+		i = -1;
+		if (node->all_cmd[1][0] == '+' || node->all_cmd[1][0] == '-')
+			i++;
+		while (node->all_cmd[1][++i])
+		{
+			if (ft_isdigit(node->all_cmd[1][i]) == 0)
+			{
+				ft_printf(fd, "error: exit");
+				break ;
+			}
+		}
+	}
+	if (data->path != NULL)
+	{
+		free_split(data->path);
+		data->path = NULL;
+	}
+	exit(g_status);
+}
